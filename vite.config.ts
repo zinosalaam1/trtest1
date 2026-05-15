16373ff4db4f56f085a54fdd16e927ccfebbe6b9
zinosalaam1 <bolatitosalaam7@gmail.com>
2026-05-15 15:04:45 +0100

    init

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-import svgr from '@svgr/vite'
+import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
   plugins: [
